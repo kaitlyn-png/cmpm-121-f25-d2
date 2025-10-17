10f33caad93b41035a7043cb2ef06123027243b0
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2025-10-17 08:41:07 +0000

    created canvas and styled with css

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <!--<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>-->
+
+  <div id = "main">
+    <p id = "header"> Sticker Sketchpad </p>
+    <canvas id="cvs"></canvas>
+  </div>
 `;
+
+const canvas = document.getElementById("cvs") as HTMLCanvasElement;
+canvas.id = "cvs";
+const context = canvas.getContext("2d");
+
+if (context) {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
