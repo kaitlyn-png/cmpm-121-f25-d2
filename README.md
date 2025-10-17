c96e81832ba50088c64f7b50794111518a01bb92
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2025-10-17 08:43:57 +0000

    cleaned up main.ts code (step 1 complete)

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # CMPM 121 D2 Project
 
 Sticker sketchpad project. Hold down left mouse button to draw. Use the buttons to undo/redo and edit your sketch that you drew.
- 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <!--<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>-->
-
   <div id = "main">
     <p id = "header"> Sticker Sketchpad </p>
     <canvas id="cvs"></canvas>
