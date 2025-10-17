504c539929862af7d5f1351580d457177defea7e
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2025-10-17 11:41:51 +0000

    allows user to draw on canvas + added a clear button + styled everything in css

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import "./style.css";
 
 document.body.innerHTML = `
   <div id = "main">
-    <p id = "header"> Sticker Sketchpad </p>
-    <canvas id="cvs"></canvas>
+    <div id = "sketchpad-container">
+      <p id = "header"> Sticker Sketchpad </p>
+      <canvas id="cvs"></canvas>
+    </div>
+    <div id = "buttons-container">
+      <button id="clear-button">clear</button>
+    </div>
   </div>
 `;
 
@@ -15,3 +20,41 @@ if (context) {
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+const cursor = {
+  active: false,
+  x: 0,
+  y: 0,
+};
+
+canvas.addEventListener("mousedown", (event) => {
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (cursor.active && context) {
+    context.beginPath();
+    context.moveTo(cursor.x, cursor.y);
+    context.lineTo(event.offsetX, event.offsetY);
+    context.stroke();
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+    context.lineWidth = 2;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.getElementById("clear-button") as HTMLButtonElement;
+
+clearButton.addEventListener("click", () => {
+  if (context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+});
